f490eb7cb690b3f28f9348c98b628a1e1b54979d
Shashank Dubey <jfriday464@gmail.com>
2025-02-07 18:18:08 +0530

    Deault log fixes

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_WORKSPACE_LOGO =
-  'https://twentyhq.github.io/placeholder-images/workspaces/twenty-logo.png';
+  'https://app.joinaston.com/icons/android/android-launchericon-192-192.png';
