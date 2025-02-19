ed56b1177fcaebb70da4e4d814c038fbf090e593
yogeshdev36 <dev36@bytescrum.com>
2025-02-19 11:31:21 +0530

    url cchanges

--- packages/twenty-front/src/modules/settings/components/SettingsNavigationDrawerItems.tsx
+++ packages/twenty-front/src/modules/settings/components/SettingsNavigationDrawerItems.tsx
@@ -143,7 +143,7 @@ export const SettingsNavigationDrawerItems = () => {
           label={t`General`}
           path={SettingsPath.Workspace}
           Icon={IconSettings}
-          onClick={() => window.open('https://www.joinaston.com/user-guide', '_blank')}
+          
         />
         <SettingsNavigationDrawerItem
           label={t`Members`}
@@ -173,14 +173,14 @@ export const SettingsNavigationDrawerItems = () => {
           label={t`Integrations`}
           path={SettingsPath.Integrations}
           Icon={IconApps}
-          onClick={() => window.open('https://www.joinaston.com/', '_blank')}
+           
         />
         <AdvancedSettingsWrapper navigationDrawerItem={true}>
           <SettingsNavigationDrawerItem
             label={t`Security`}
             path={SettingsPath.Security}
             Icon={IconKey}
-            onClick={() => window.open('https://www.joinaston.com/user-guide', '_blank')}
+  
           />
         </AdvancedSettingsWrapper>
       </NavigationDrawerSection>
@@ -194,7 +194,7 @@ export const SettingsNavigationDrawerItems = () => {
             label={t`API & Webhooks`}
             path={SettingsPath.Developers}
             Icon={IconCode}
-            onClick={() => window.open('https://www.joinaston.com/user-guide', '_blank')}
+             
           />
         </AdvancedSettingsWrapper>
         {isFunctionSettingsEnabled && (
